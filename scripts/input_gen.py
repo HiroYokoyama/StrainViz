25e403618cdc089f9c4008c1eb8939f4c7a8ac31
Hiromichi Yokoyama <58853434+HiroYokoyama@users.noreply.github.com>
2026-06-28 18:47:30 +0900

    Add files via upload

--- scripts/input_gen.py
+++ scripts/input_gen.py
@@ -31,7 +31,7 @@ def create_input(file):
 
 	script.write("! " + functional + " OPT " + basis +"\n")
 	script.write("%pal\n\tnprocs " + processors + "\nend\n")
-	script.write("%geom\n\tMaxStep 0.15\nend\n")
+	script.write("%geom\n\tMaxStep 0.15\n\tMaxIter 512\nend\n")
 	script.write("* xyz 0 1\n")
 	for line in coordinates:
 		script.write(line + "\n")
